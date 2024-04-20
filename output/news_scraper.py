222cb0ed30a6dcf523021bc1a1a972de1b78f550
Vinex7 <84782007+Vinex7@users.noreply.github.com>
2024-04-20 16:39:06 +0200

    Update news_scraper.py

--- output/news_scraper.py
+++ output/news_scraper.py
@@ -1,8 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 
+# Define the URL of the website to scrape
 news_url = "https://www.aljazeera.com/"
 
+# Send an HTTP GET request to the website
 response = requests.get(news_url)
+
+# Create a BeautifulSoup object to parse the HTML content
 soup = BeautifulSoup(response.content, 'html.parser')
+
+# Find specific elements in the HTML content using BeautifulSoup
+# For example, if you want to find all the headlines on the page:
+headlines = soup.find_all('h2', class_='top-sec-title')
+
+# Print out the headlines
+for headline in headlines:
+    print(headline.text.strip())
 
