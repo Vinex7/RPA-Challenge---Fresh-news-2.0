b5f2a6f68f0c9ef622885c8114c94154295b5e51
Vinex7 <84782007+Vinex7@users.noreply.github.com>
2024-04-20 16:36:06 +0200

    Update news_scraper.py

--- output/news_scraper.py
+++ output/news_scraper.py
@@ -1,2 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+
+news_url = "https://www.aljazeera.com/"
+
+response = requests.get(news_url)
+soup = BeautifulSoup(response.content, 'html.parser')
+
